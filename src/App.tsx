69ee8c86af52bdd6d6126ecd5058d5d6c35a01ee
Gregor Majcen <gregor.majcen@sinergise.com>
2022-03-08 17:02:15 +0100

    force darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ function App() {
   };
 
   return (
-    <>
+    <div className="dark">
       <ToastContainer />
       <Table data={data} />
 
@@ -115,7 +115,7 @@ function App() {
         <br></br>
         <Button text="Izvoz" onClick={onCopyClick} />
       </div>
-    </>
+    </div>
   );
 }
 
